e828accba57fc2abc448bc4a8764088b99fea7ac
carlosrsantos <66956614+carlosrsantos@users.noreply.github.com>
2021-03-29 16:36:39 -0300

    Uploding

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -5,14 +5,57 @@ import cronometro from './assets/cronometro.png';
 import './style.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      numero: 0, 
+      botao: 'INICIAR'
+    };
+    this.timer = null;
+    this.iniciar = this.iniciar.bind(this);
+    this.zerar = this.zerar.bind(this);
+  }
+
+  iniciar(){
+      let state = this.state;
+
+      if(this.timer !== null){
+        clearInterval(this.timer);
+        this.timer = null;
+        state.botao = 'INICIAR';
+      }else{
+        this.timer = setInterval(()=>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100);
+      state.botao = "PAUSAR";
+    }
+
+    this.setState(state);
+  }
+
+  zerar(){
+      if(this.timer !== null){
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+
+      let state = this.state;
+      state.numero = 0;
+      state.botao = 'INICIAR';
+      this.setState(state);
+  }
+
   render(){
     return (
       <div className="container">
       <img src={cronometro} className="img" />
-      <a className="timer">0.0</a> 
+      <a className="timer">{ this.state.numero.toFixed(1) }</a> 
       <div className="areaBtn">
-        <a className="botao">VAI</a>
-        <a className="botao">LIMPAR</a> 
+        <a className="botao" onClick={ this.iniciar }>{ this.state.botao }</a>
+        <a className="botao" onClick={ this.zerar }>ZERAR</a> 
       </div>
       </div>
     );
